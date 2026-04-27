0cfc23ec8be7953c440046c1d6ad7dce70547859
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-27 15:53:20 +0530

    Configure auth service connection client setup

--- auth-service/src/main/resources/AuthServiceApplication.java
+++ auth-service/src/main/resources/AuthServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class AuthServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
 }
