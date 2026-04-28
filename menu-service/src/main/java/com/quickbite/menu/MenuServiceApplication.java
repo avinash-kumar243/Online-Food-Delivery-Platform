b86c6f76a07faf2abeb547f6ef77599b3c2b1b2c
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-28 18:57:43 +0530

    [AVINASH] : Updated menu service application configuration

--- menu-service/src/main/java/com/quickbite/menu/MenuServiceApplication.java
+++ menu-service/src/main/java/com/quickbite/menu/MenuServiceApplication.java
@@ -3,9 +3,11 @@ package com.quickbite.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableCaching
 @SpringBootApplication
+@EnableFeignClients
 public class MenuServiceApplication {
 
     public static void main(String[] args) {
