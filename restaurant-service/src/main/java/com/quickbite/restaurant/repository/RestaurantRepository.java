3217becd1eaf4accd246fac48edd96e279581304
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-28 18:58:12 +0530

    [AVINASH] : Updated restaurant repository queries

--- restaurant-service/src/main/java/com/quickbite/restaurant/repository/RestaurantRepository.java
+++ restaurant-service/src/main/java/com/quickbite/restaurant/repository/RestaurantRepository.java
@@ -4,17 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.quickbite.restaurant.entity.ApprovalStatus;
 import com.quickbite.restaurant.entity.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findByOwnerId(Long ownerId);
 
+    java.util.Optional<Restaurant> findFirstByOwnerIdOrderByRestaurantIdAsc(Long ownerId);
+
     List<Restaurant> findByCuisine(String cuisine);
 
     List<Restaurant> findByCity(String city);
 
     List<Restaurant> findByIsOpenTrueAndIsApprovedTrue();
+
+    List<Restaurant> findByApprovalStatus(ApprovalStatus approvalStatus);
+
+    List<Restaurant> findByApprovalStatusAndIsApprovedTrue(ApprovalStatus approvalStatus);
 
     List<Restaurant> findByNameContaining(String name);
 }
