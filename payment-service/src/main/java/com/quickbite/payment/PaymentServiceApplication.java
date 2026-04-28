c51f40703a371ee49d498be0bd4412124d23b3fe
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-28 18:57:56 +0530

    [AVINASH] : Updated payment service application configuration

--- payment-service/src/main/java/com/quickbite/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/quickbite/payment/PaymentServiceApplication.java
@@ -3,8 +3,10 @@ package com.quickbite.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@EnableFeignClients
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
