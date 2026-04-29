8c325c9a4b7dfa9442dc3c2b29aa8f56bed7e7dc
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-29 16:35:07 +0530

    [AVINASH] : Updated RabbitMQ configuration in notification service

--- notification-service/src/main/java/com/quickbite/notification/config/RabbitMqConfig.java
+++ notification-service/src/main/java/com/quickbite/notification/config/RabbitMqConfig.java
@@ -1,29 +1,72 @@
 package com.quickbite.notification.config;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
-    public static final String ORDER_EVENTS_QUEUE = "order_events";
-    public static final String DELIVERY_EVENTS_QUEUE = "delivery_events";
-
-    @Bean
-    public Queue orderEventsQueue() {
-        return new Queue(ORDER_EVENTS_QUEUE, true);
-    }
-
-    @Bean
-    public Queue deliveryEventsQueue() {
-        return new Queue(DELIVERY_EVENTS_QUEUE, true);
-    }
+    public static final String ORDER_EXCHANGE = "quickbite.order.exchange";
+    public static final String ORDER_DLX = "quickbite.order.dlx.exchange";
+    public static final String NOTIFICATION_QUEUE = "quickbite.notification-service.all-events";
 
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public TopicExchange quickbiteOrderExchange() {
+        return new TopicExchange(ORDER_EXCHANGE, true, false);
+    }
+
+    @Bean
+    public TopicExchange quickbiteOrderDeadLetterExchange() {
+        return new TopicExchange(ORDER_DLX, true, false);
+    }
+
+    @Bean
+    public Declarables notificationDeclarables(TopicExchange quickbiteOrderExchange, TopicExchange quickbiteOrderDeadLetterExchange) {
+        var notificationQueue = QueueBuilder.durable(NOTIFICATION_QUEUE)
+            .withArguments(deadLetterArguments(NOTIFICATION_QUEUE + ".dlq"))
+            .build();
+        var notificationDlq = QueueBuilder.durable(NOTIFICATION_QUEUE + ".dlq").build();
+        return new Declarables(
+            notificationQueue,
+            notificationDlq,
+            BindingBuilder.bind(notificationQueue).to(quickbiteOrderExchange).with("#"),
+            BindingBuilder.bind(notificationDlq).to(quickbiteOrderDeadLetterExchange).with(notificationDlq.getName())
+        );
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory manualAckRabbitListenerContainerFactory(
+        SimpleRabbitListenerContainerFactoryConfigurer configurer,
+        ConnectionFactory connectionFactory,
+        MessageConverter messageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
+
+    private Map<String, Object> deadLetterArguments(String deadLetterQueueName) {
+        Map<String, Object> arguments = new LinkedHashMap<>();
+        arguments.put("x-dead-letter-exchange", ORDER_DLX);
+        arguments.put("x-dead-letter-routing-key", deadLetterQueueName);
+        return arguments;
     }
 }
