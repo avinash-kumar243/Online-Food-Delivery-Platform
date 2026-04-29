2b31e64dad18682754f41b373df97f1f602497ec
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-29 16:35:12 +0530

    [AVINASH] : Updated order DTO in review service

--- review-service/src/main/java/com/quickbite/review/client/dto/OrderDto.java
+++ review-service/src/main/java/com/quickbite/review/client/dto/OrderDto.java
@@ -1,5 +1,7 @@
 package com.quickbite.review.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import lombok.Data;
 
 @Data
@@ -7,5 +9,6 @@ public class OrderDto {
     private Long orderId;
     private Long customerId;
     private Long restaurantId;
+    @JsonAlias("deliveryAgentId")
     private Long agentId;
 }
