f3824cbca8cab06d7a23b6dd08e82d9adf9115d6
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-29 16:35:12 +0530

    [AVINASH] : Added RabbitMQ messaging package in review service

---
+++ review-service/src/main/java/com/quickbite/review/messaging/QuickbiteOrderMessagingConstants.java
@@ -0,0 +1,11 @@
+package com.quickbite.review.messaging;
+
+public final class QuickbiteOrderMessagingConstants {
+
+    public static final String ORDER_EXCHANGE = "quickbite.order.exchange";
+    public static final String ORDER_DLX = "quickbite.order.dlx.exchange";
+    public static final String ORDER_DELIVERED_QUEUE = "quickbite.review-service.order-delivered";
+
+    private QuickbiteOrderMessagingConstants() {
+    }
+}
---
+++ review-service/src/main/java/com/quickbite/review/messaging/RabbitMqConfig.java
@@ -0,0 +1,68 @@
+package com.quickbite.review.messaging;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    MessageConverter jacksonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    TopicExchange quickbiteOrderExchange() {
+        return new TopicExchange(QuickbiteOrderMessagingConstants.ORDER_EXCHANGE, true, false);
+    }
+
+    @Bean
+    TopicExchange quickbiteOrderDeadLetterExchange() {
+        return new TopicExchange(QuickbiteOrderMessagingConstants.ORDER_DLX, true, false);
+    }
+
+    @Bean
+    Declarables reviewDeclarables(TopicExchange quickbiteOrderExchange, TopicExchange quickbiteOrderDeadLetterExchange) {
+        var deliveredQueue = QueueBuilder.durable(QuickbiteOrderMessagingConstants.ORDER_DELIVERED_QUEUE)
+            .withArguments(deadLetterArguments(QuickbiteOrderMessagingConstants.ORDER_DELIVERED_QUEUE + ".dlq"))
+            .build();
+        var deliveredDlq = QueueBuilder.durable(QuickbiteOrderMessagingConstants.ORDER_DELIVERED_QUEUE + ".dlq").build();
+        return new Declarables(
+            deliveredQueue,
+            deliveredDlq,
+            BindingBuilder.bind(deliveredQueue).to(quickbiteOrderExchange).with("order.delivered"),
+            BindingBuilder.bind(deliveredDlq).to(quickbiteOrderDeadLetterExchange).with(deliveredDlq.getName())
+        );
+    }
+
+    @Bean
+    SimpleRabbitListenerContainerFactory manualAckRabbitListenerContainerFactory(
+        SimpleRabbitListenerContainerFactoryConfigurer configurer,
+        ConnectionFactory connectionFactory,
+        MessageConverter jacksonMessageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setMessageConverter(jacksonMessageConverter);
+        return factory;
+    }
+
+    private Map<String, Object> deadLetterArguments(String deadLetterQueueName) {
+        Map<String, Object> arguments = new LinkedHashMap<>();
+        arguments.put("x-dead-letter-exchange", QuickbiteOrderMessagingConstants.ORDER_DLX);
+        arguments.put("x-dead-letter-routing-key", deadLetterQueueName);
+        return arguments;
+    }
+}
