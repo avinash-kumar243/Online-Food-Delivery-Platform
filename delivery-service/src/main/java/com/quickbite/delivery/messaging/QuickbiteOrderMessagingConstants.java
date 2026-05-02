f93ed471398724010e1565a3891218ce78ceef79
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-02 14:03:16 +0530

    [AVINASH] : Updated delivery messaging constants

--- delivery-service/src/main/java/com/quickbite/delivery/messaging/QuickbiteOrderMessagingConstants.java
+++ delivery-service/src/main/java/com/quickbite/delivery/messaging/QuickbiteOrderMessagingConstants.java
@@ -5,6 +5,7 @@ public final class QuickbiteOrderMessagingConstants {
     public static final String ORDER_EXCHANGE = "quickbite.order.exchange";
     public static final String ORDER_DLX = "quickbite.order.dlx.exchange";
     public static final String RESTAURANT_ACCEPTED_QUEUE = "quickbite.delivery-service.restaurant-accepted";
+    public static final String ORDER_DELIVERED_QUEUE = "quickbite.delivery-service.order-delivered";
 
     private QuickbiteOrderMessagingConstants() {
     }
