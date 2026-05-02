0675d6a95939f3f6682a7c3b33e431dc4ac99269
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-02 14:21:09 +0530

    [AVINASH] : Updated restaurant payment event DTO

--- restaurant-service/src/main/java/com/quickbite/restaurant/messaging/dto/PaymentEventDTO.java
+++ restaurant-service/src/main/java/com/quickbite/restaurant/messaging/dto/PaymentEventDTO.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 
 public record PaymentEventDTO(
     Long orderId,
+    Long customerId,
+    Long restaurantId,
+    Long deliveryAgentId,
     String transactionId,
     String status,
     BigDecimal amount
