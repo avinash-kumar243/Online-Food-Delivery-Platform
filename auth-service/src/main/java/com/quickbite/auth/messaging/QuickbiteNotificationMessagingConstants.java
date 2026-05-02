0fb1ce74461f78550c69f661689c49451c78f98f
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-02 14:01:31 +0530

    [AVINASH] : Added auth service messaging layer

---
+++ auth-service/src/main/java/com/quickbite/auth/messaging/QuickbiteNotificationMessagingConstants.java
@@ -0,0 +1,11 @@
+package com.quickbite.auth.messaging;
+
+public final class QuickbiteNotificationMessagingConstants {
+
+    public static final String NOTIFICATION_EXCHANGE = "quickbite.notification.exchange";
+    public static final String NOTIFICATION_DLX = "quickbite.notification.dlx.exchange";
+    public static final String PASSWORD_RESET_OTP_ROUTING_KEY = "auth.password-reset-otp";
+
+    private QuickbiteNotificationMessagingConstants() {
+    }
+}
