3ba5db136bab8ba2621f39adeb6743dc5b68dd1f
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-02 14:20:46 +0530

    [AVINASH] : Updated payment service order client

--- payment-service/src/main/java/com/quickbite/payment/client/OrderServiceClient.java
+++ payment-service/src/main/java/com/quickbite/payment/client/OrderServiceClient.java
@@ -2,13 +2,18 @@ package com.quickbite.payment.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.quickbite.payment.dto.OrderSnapshotDto;
 import com.quickbite.payment.dto.OrderPaymentStatusRequest;
 
 @FeignClient(name = "order-service", path = "/api/v1/orders")
 public interface OrderServiceClient {
+
+    @GetMapping("/{orderId}")
+    OrderSnapshotDto getOrderById(@PathVariable("orderId") Long orderId);
 
     @PutMapping("/{orderId}/payment-status")
     void updateOrderPaymentStatus(@PathVariable("orderId") Long orderId, @RequestBody OrderPaymentStatusRequest request);
