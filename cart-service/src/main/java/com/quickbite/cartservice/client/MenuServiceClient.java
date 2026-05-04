1eb0eac93b03da5fa52f58acf6136b32cb5553d3
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-04 11:27:51 +0530

    [AVINASH] : Added Feign client for fetching menu item details in cart-service

--- cart-service/src/main/java/com/quickbite/cartservice/client/MenuServiceClient.java
+++ cart-service/src/main/java/com/quickbite/cartservice/client/MenuServiceClient.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.quickbite.cartservice.dto.MenuItemSnapshotDto;
 
-@FeignClient(name = "menu-service", path = "/menu")
+@FeignClient(name = "menu-service", path = "/api/v1/menu")
 public interface MenuServiceClient {
 
     @GetMapping("/item/{itemId}")
     MenuItemSnapshotDto getMenuItem(@PathVariable("itemId") Integer itemId);
 }
+
