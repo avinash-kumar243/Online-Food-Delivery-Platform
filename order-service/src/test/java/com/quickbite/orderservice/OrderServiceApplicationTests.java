0d2c7babda1d57e5842b0c83b632aeb6bea8188c
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-08 14:55:53 +0530

    Update order service application tests

--- order-service/src/test/java/com/quickbite/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/quickbite/orderservice/OrderServiceApplicationTests.java
@@ -5,7 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
     "eureka.client.enabled=false",
-    "spring.boot.admin.client.enabled=false"
+    "spring.boot.admin.client.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:orderservice;DB_CLOSE_DELAY=-1;MODE=MySQL",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class OrderServiceApplicationTests {
 
