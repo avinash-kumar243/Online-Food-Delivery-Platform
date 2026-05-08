11bb8fc7837fb9d723ee8c6fe85ebc5e0d5445aa
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-08 14:55:48 +0530

    Update delivery service application tests

--- delivery-service/src/test/java/com/quickbite/delivery/DeliveryServiceApplicationTests.java
+++ delivery-service/src/test/java/com/quickbite/delivery/DeliveryServiceApplicationTests.java
@@ -5,7 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
 	"eureka.client.enabled=false",
-	"spring.boot.admin.client.enabled=false"
+	"spring.boot.admin.client.enabled=false",
+	"spring.cloud.compatibility-verifier.enabled=false",
+	"spring.datasource.url=jdbc:h2:mem:deliveryservice;DB_CLOSE_DELAY=-1;MODE=MySQL",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class DeliveryServiceApplicationTests {
 
