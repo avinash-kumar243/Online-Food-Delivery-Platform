192839913fc35654791238742f940cfd3e45791a
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-08 14:56:00 +0530

    Update review service application tests

--- review-service/src/test/java/com/quickbite/review/ReviewServiceApplicationTests.java
+++ review-service/src/test/java/com/quickbite/review/ReviewServiceApplicationTests.java
@@ -6,7 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(properties = {
         "eureka.client.enabled=false",
         "spring.cloud.discovery.enabled=false",
-        "spring.boot.admin.client.enabled=false"
+        "spring.cloud.compatibility-verifier.enabled=false",
+        "spring.boot.admin.client.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:reviewservice;DB_CLOSE_DELAY=-1;MODE=MySQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class ReviewServiceApplicationTests {
 
