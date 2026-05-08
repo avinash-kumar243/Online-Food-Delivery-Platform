ff8c1f97a96147e54cf09c9b2e6f672fba689618
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-09 02:41:36 +0530

    Update customer profile update DTO

--- auth-service/src/main/java/com/quickbite/auth/dto/CustomerUpdateProfileDto.java
+++ auth-service/src/main/java/com/quickbite/auth/dto/CustomerUpdateProfileDto.java
@@ -8,5 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerUpdateProfileDto {
+	private String fullName;
+	private String phone;
 	private String profilePicUrl;
 }
