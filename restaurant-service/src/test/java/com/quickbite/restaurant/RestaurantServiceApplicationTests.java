d0d37f82f5c62d76ad4e11180635163c226ca37d
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-08 14:55:56 +0530

    Update restaurant service application tests

--- restaurant-service/src/test/java/com/quickbite/restaurant/RestaurantServiceApplicationTests.java
+++ restaurant-service/src/test/java/com/quickbite/restaurant/RestaurantServiceApplicationTests.java
@@ -5,7 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
     "eureka.client.enabled=false",
-    "spring.boot.admin.client.enabled=false"
+    "spring.boot.admin.client.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:restaurantdb;DB_CLOSE_DELAY=-1;MODE=MYSQL",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class RestaurantServiceApplicationTests {
 
