2ef8bc2619d1dcf7ac1985184276e4a967b758b3
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-17 17:19:31 +0530

    [AVINASH] : Added IllegalOperationException to block unsafe admin self-operations

---
+++ auth-service/src/main/java/com/quickbite/auth/exception/IllegalOperationException.java
@@ -0,0 +1,8 @@
+package com.quickbite.auth.exception;
+
+public class IllegalOperationException extends RuntimeException {
+
+    public IllegalOperationException(String message) {
+        super(message);
+    }
+}
