51cd72a54a11426019593f95a4771d14281e7517
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-17 14:32:42 +0530

    [AVINASH] : Added realtime restaurant DTO support

--- order-service/src/main/java/com/quickbite/orderservice/client/dto/RestaurantRealtimeDto.java
+++ order-service/src/main/java/com/quickbite/orderservice/client/dto/RestaurantRealtimeDto.java
@@ -4,6 +4,9 @@ public record RestaurantRealtimeDto(
     Long restaurantId,
     Long ownerId,
     String name,
+    String phone,
+    String address,
+    String city,
     Boolean isOpen,
     Boolean isApproved
 ) {
