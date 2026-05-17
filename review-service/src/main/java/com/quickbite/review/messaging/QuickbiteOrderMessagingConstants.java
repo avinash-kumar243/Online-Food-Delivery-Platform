c67f07fab58ecea00e4362762786b415157bf7b1
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-17 14:32:46 +0530

    [AVINASH] : Added review notification messaging constants

--- review-service/src/main/java/com/quickbite/review/messaging/QuickbiteOrderMessagingConstants.java
+++ review-service/src/main/java/com/quickbite/review/messaging/QuickbiteOrderMessagingConstants.java
@@ -5,6 +5,7 @@ public final class QuickbiteOrderMessagingConstants {
     public static final String ORDER_EXCHANGE = "quickbite.order.exchange";
     public static final String ORDER_DLX = "quickbite.order.dlx.exchange";
     public static final String ORDER_DELIVERED_QUEUE = "quickbite.review-service.order-delivered";
+    public static final String NOTIFICATION_EXCHANGE = "quickbite.notification.exchange";
 
     private QuickbiteOrderMessagingConstants() {
     }
