af3567ab7b063d68a1c9288627fc0678350b1289
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-29 11:35:56 +0530

    Updated payment service messaging constants

--- payment-service/src/main/java/com/quickbite/payment/messaging/QuickbiteOrderMessagingConstants.java
+++ payment-service/src/main/java/com/quickbite/payment/messaging/QuickbiteOrderMessagingConstants.java
@@ -5,7 +5,9 @@ public final class QuickbiteOrderMessagingConstants {
     public static final String ORDER_EXCHANGE = "quickbite.order.exchange";
     public static final String ORDER_DLX = "quickbite.order.dlx.exchange";
     public static final String ORDER_CREATED_QUEUE = "quickbite.payment-service.order-created";
-    public static final String ORDER_DELIVERED_QUEUE = "quickbite.payment-service.order-delivered";
+    public static final String PAYMENT_COMPLETED_ROUTING_KEY = "payment.completed";
+    public static final String PAYMENT_FAILED_ROUTING_KEY = "payment.failed";
+    public static final String REFUND_INITIATED_ROUTING_KEY = "payment.refund.initiated";
 
     private QuickbiteOrderMessagingConstants() {
     }
